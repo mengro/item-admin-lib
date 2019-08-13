73b4046caa6be5f337ec9030e26e91cc03885195
mengro <550494402@qq.com>
2019-08-13 15:06:30 +0800

    xx

--- .umirc.library.ts
+++ .umirc.library.ts
@@ -3,7 +3,10 @@ import { IBundleOptions } from 'umi-library/src/types';
 const options: IBundleOptions = {
   cjs: 'rollup',
   esm: 'rollup',
-  doc: { typescript: true },
+  doc: { 
+    typescript: true,
+    base: '/item-admin-lib',
+  },
 };
 
 export default options;
